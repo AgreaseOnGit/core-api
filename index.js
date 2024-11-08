794d8d6fa9664c79251130aafd85595ad5fd50aa
irpansyah2m1d <irpansyah810@gmail.com>
2024-11-08 13:47:52 +0700

    Create Server and Connect Firebase for API

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
 const express = require("express");
 const cors = require("cors");
-const User = require("./config");
+const Item = require("./config");
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/", async (req, res) => {
-  const snapshot = await User.get();
+  const snapshot = await Item.get();
   const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   res.send(list);
 });
 
+
+app.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const doc = await Item.doc(id).get();
+
+    if (!doc.exists) {
+      return res.status(404).send({ error: "Document not found" });
+    }
+
+    const data = { id: doc.id, ...doc.data() };
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching document:", error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
 app.post("/create", async (req, res) => {
   const data = req.body;
-  await User.add({ data });
-  res.send({ msg: "User Added" });
+  await Item.add({ data });
+  res.send({ msg: "Item Added" });
 });
 
 app.post("/update", async (req, res) => {
   const id = req.body.id;
   delete req.body.id;
   const data = req.body;
-  await User.doc(id).update(data);
+  await Item.doc(id).update(data);
   res.send({ msg: "Updated" });
 });
 
 app.post("/delete", async (req, res) => {
   const id = req.body.id;
-  await User.doc(id).delete();
+  await Item.doc(id).delete();
   res.send({ msg: "Deleted" });
 });
-app.listen(4000, () => console.log("Up & RUnning *4000"));
+app.listen(8000, () => console.log("Server Started On Port 8000...."));
